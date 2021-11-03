8264baa4cd37a1ffa5b023201bbd25c47ec8707b
Aloïs Delaune <90683963+AloisD@users.noreply.github.com>
2021-11-03 18:38:03 +0100

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -6,51 +6,51 @@ $name = $firstname = $email = $tel = $message = "";
 const BR = '<br> <br> <br>';
 
 if (empty($_POST["user_name"])) {
-    $errors ["nameErr"] = "Name is required";
+    $errors["nameErr"] = "Name is required";
 } else {
     $name = test_input($_POST["user_name"]);
     if (!preg_match("/^[a-zA-Z-' ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ]*$/",$name)) {
-        $errors ["nameErr"] = "Only letters and white space are allowed for name";
+        $errors["nameErr"] = "Only letters and white space are allowed for name";
     }
 }
 
 if (empty($_POST["user_firstname"])) {
-    $errors ["firstname"] = "Firstname is required";
+    $errors["firstname"] = "Firstname is required";
 } else {
     $firstname = test_input($_POST["user_firstname"]);
     if (!preg_match("/^[a-zA-Z-' ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ]*$/",$firstname)) {
-        $errors ["firstname"] = "Only letters and white space are allowed for firstname";
+        $errors["firstname"] = "Only letters and white space are allowed for firstname";
     }
 }
 
 if (empty($_POST["user_email"])) {
-    $errors ["emailErr"] = "Email is required";
+    $errors["emailErr"] = "Email is required";
 } else {
     $email = test_input($_POST["user_email"]);
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors ["emailErr"] = "Invalid email format";
+        $errors["emailErr"] = "Invalid email format";
     }
 }
 
 if (empty($_POST["user_tel"])) {
-    $errors ["telErr"] = "Phone number is required";
+    $errors["telErr"] = "Phone number is required";
 } else {
     $tel = test_input($_POST["user_tel"]);
     if (!preg_match("/^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/",$tel)) {
-        $errors ["telErr"] = "Invalide phone number format";
+        $errors["telErr"] = "Invalide phone number format";
     }
 }
 
 if ($_POST["user_message_subject"] == "Sujet") {
-    $errors ["subjectErr"] = "Please choose a subject";
+    $errors["subjectErr"] = "Please choose a subject";
 }
 
 if (empty($_POST["user_message"])) {
-    $errors ["messageErr"] = "A message is required";
+    $errors["messageErr"] = "A message is required";
 } else {
     $message = test_input($_POST["user_message"]);
     if (!preg_match("/^[a-zA-Z-' ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ]*$/",$message)) {
-        $errors ["messageErr"] = "Only letters and white space allowed";
+        $errors["messageErr"] = "Only letters and white space allowed";
     }
 }
 
